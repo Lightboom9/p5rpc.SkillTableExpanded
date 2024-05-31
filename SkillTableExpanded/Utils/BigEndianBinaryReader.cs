ee2b109d355a1174fb4f183406d9f52ace4e3f30
Lightboom <55315954+Lightboom9@users.noreply.github.com>
2024-05-31 03:29:37 +0300

    Added expanded table file load

---
+++ SkillTableExpanded/Utils/BigEndianBinaryReader.cs
@@ -0,0 +1,37 @@
+﻿namespace SkillTableExpanded.Utils;
+
+public class BigEndianBinaryReader : IDisposable
+{
+    private readonly BinaryReader _reader;
+    
+    public BigEndianBinaryReader(Stream input)
+    {
+        _reader = new BinaryReader(input);
+    }
+
+    public byte[] ReadBytes(int count) => _reader.ReadBytes(count);
+    public byte[] ReadBytesAndReverse(int count) => Reverse(_reader.ReadBytes(count));
+    public byte ReadByte() => _reader.ReadByte();
+    public short ReadInt16() => BitConverter.ToInt16(Reverse(_reader.ReadBytes(2)));
+    public ushort ReadUInt16() => BitConverter.ToUInt16(Reverse(_reader.ReadBytes(2)));
+    public int ReadInt32() => BitConverter.ToInt32(Reverse(_reader.ReadBytes(4)));
+    public uint ReadUInt32() => BitConverter.ToUInt32(Reverse(_reader.ReadBytes(4)));
+    
+    private byte[] Reverse(byte[] arr)
+    {
+        var reversedArr = new byte[arr.Length];
+        var lastIndex = arr.Length - 1;
+
+        for (var i = 0; i <= lastIndex; i++)
+        {
+            reversedArr[i] = arr[lastIndex - i];
+        }
+
+        return reversedArr;
+    }
+
+    public void Dispose()
+    {
+        _reader.Dispose();
+    }
+}
